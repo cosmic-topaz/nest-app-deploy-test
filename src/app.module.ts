7e3cc68ba97df433fcffba22505e0d396785a401
Lee <windshul@gmail.com>
2025-06-09 22:56:34 +0900

    chore: update env file path setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 
+const ENV = process.env.NODE_ENV;
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env', // backend/.env 기준
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
     }),
     PrismaModule,
     UsersModule,
